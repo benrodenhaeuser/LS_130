301752c8ec333bfb9f89612ca5044ba639cdf191
Ben Rodenhäuser <ben@karo.berlin>
2017-11-12 14:34:22 +0100

    minor

--- 01_blocks/test.rb
+++ 01_blocks/test.rb
@@ -1,6 +1,4 @@
-def compare(str)
-  puts "Before: #{str}"
-  puts "After: #{yield(str)}"
-end
-
-compare('hi') { |word| word.upcase }
+name = 'ben'
+proc = Proc.new { puts name } 
+name = 'camillo'
+proc.call # output: camillo
